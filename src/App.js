0e68d2759ef4c4fb72aa541fd6a864d27a3b65ec
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-25 21:25:22 +0600

    add layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from './components/Layout/Layout'
+import Burgerbuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <p>Hello Test</p>
+        <Burgerbuilder />
       </Layout>
     </div>
   );
