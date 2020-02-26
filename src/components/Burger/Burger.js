885126ea129f9da397d7e472f1bda286bd1212c8
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-26 22:31:28 +0600

    Burger layer dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import BurgerInGredient from './BurgerInGradient/BurgerInGradient'
 import './Burger.css'
 
 const burger = (props) => {
-    const transformInGredients = Object.keys(props.ingredients).map(igKey => {
+    let transformInGredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerInGredient key={igKey + i} type={igKey} />
         })
-    })
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, [])
+
+    if(transformInGredients.length === 0) {
+        transformInGredients = <p>Please select adding ingredient!</p>
+    }
     return(
         <div className="burger">
             <BurgerInGredient type="bread-top" />
