252fb0440337ebd537de49c77443ce5730db6902
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-27 23:16:24 +0600

    passing type into add or remove function

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import './BuildControls.css'
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import './BuildControls.css';
 
-const buildControls = (props) => {
-    <div>
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
 
+const buildControls = props => {
+  return (
+    <div className="buildControls">
+      {controls.map((control, index) => (
+        <BuildControl
+          added={() => props.ingredientAdded(control.type)}
+          remove={() => props.ingredientRemove(control.type)}
+          key={index}
+          label={control.label}
+        />
+      ))}
     </div>
-}
+  );
+};
 
 export default buildControls;
