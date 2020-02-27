be5690d84d55ec050baddb63c25ac9126b05a559
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-27 23:15:39 +0600

    Added less and more click function

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import './buildControl.css'
+import React from 'react';
+import './BuildControl.css';
 
-const buildControl = (props) => (
-    <div className="buildControl">
-        <div className="label">{props.label}</div>
-        <button className="less">Less</button>
-        <button className="more">More</button>
-    </div>
-)
+const buildControl = props => (
+  <div className="buildControl">
+    <div className="label">{props.label}</div>
+    <button className="less" onClick={props.remove}>
+      Less
+    </button>
+    <button className="more" onClick={props.added}>
+      More
+    </button>
+  </div>
+);
 
 export default buildControl;
