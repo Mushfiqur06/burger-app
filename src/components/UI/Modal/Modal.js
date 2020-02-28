f317abaf80fbcaaa26ae0a3538619759d7ca57d4
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-28 23:56:17 +0600

    Add show and hide style

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Modal.css';
 
-const modal = props => <div className="modal">{props.children}</div>;
+const modal = props => (
+  <div
+    className="modal"
+    style={{
+      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+      opacity: props.show ? '1' : '0'
+    }}
+  >
+    {props.children}
+  </div>
+);
 
 export default modal;
