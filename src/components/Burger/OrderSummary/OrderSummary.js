f67495d9a48983e523efb203fec5ce67d7a36998
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-29 01:27:41 +0600

    Add Continue and Cancel Button

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './../../UI/Button/Button';
 import Aux from './../../../hoc/Aux';
 
 const orderSummary = props => {
@@ -18,6 +19,12 @@ const orderSummary = props => {
       <p>A delicious burger with the following ingredients: </p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to checkout?</p>
+      <Button buttonType="danger" clicked={props.purchaseCanceled}>
+        Cancel
+      </Button>
+      <Button buttonType="success" clicked={props.purchaseContinued}>
+        Continue
+      </Button>
     </Aux>
   );
 };
