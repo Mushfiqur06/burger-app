cea96318c5d4f6f0b6db64c4c843d47a6e78683b
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-29 21:58:17 +0600

    Create Navbar Component for menu

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Toolbar.css';
+
+const toolbar = () => (
+  <header className="toolbar">
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>...</nav>
+  </header>
+);
+
+export default toolbar;
